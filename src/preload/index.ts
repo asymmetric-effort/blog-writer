2badf6e8107ddb50aefc9a21954b6327e42b9894
Sam Caldwell <github@samcaldwell.net>
2025-08-15 18:59:16 -0500

    resetting expectation to use go+wails+ts

--- src/preload/index.ts
+++
@@ -1,3 +0,0 @@
-// Copyright 2024 Blog Writer contributors
-// Preload script
-console.log('preload loaded');
--- src/renderer/App.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Blog Writer contributors
-import React from 'react';
-
-/** Root application component. */
-export function App(): JSX.Element {
-  return <h1>Blog Writer</h1>;
-}
-
-export default App;
