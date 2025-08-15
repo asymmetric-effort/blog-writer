922f660c95965b6fe0796011bae8466ab9f34709
Sam Caldwell <github@samcaldwell.net>
2025-08-15 17:52:30 -0500

    test: ensure Vite dev server accessible

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2024 Blog Writer
+ *
+ * @fileoverview Vite configuration exposing dev server to all interfaces.
+ */
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  }
+});
