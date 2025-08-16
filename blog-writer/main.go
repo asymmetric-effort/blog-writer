bad55b275ed45e1ab3fc586d1671a06cdae977c4
Sam Caldwell <github@samcaldwell.net>
2025-08-15 20:27:09 -0500

    fix wails load issue

--- blog-writer/main.go
+++ blog-writer/main.go
@@ -2,13 +2,14 @@
 package main
 
 import (
-  "embed"
+	"embed"
 
-  "github.com/wailsapp/wails/v2/pkg/menu"
-  "github.com/wailsapp/wails/v2/pkg/options"
-  "github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	wails "github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 
-  "blog-writer/internal/services"
+	"blog-writer/internal/services"
 )
 
 //go:embed all:frontend/dist
