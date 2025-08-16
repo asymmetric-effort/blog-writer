becbeed2888225f3212450c93445ad83b8d52549
Sam Caldwell <github@samcaldwell.net>
2025-08-15 20:11:39 -0500

    Add tests for about info

--- blog-writer/app.go
+++ blog-writer/app.go
@@ -1,27 +1,41 @@
+// Copyright (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 package main
 
 import (
 	"context"
 	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+
+	"blog-writer/internal/about"
 )
 
-// App struct
+// App holds application state.
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
+// NewApp creates a new App application struct.
 func NewApp() *App {
 	return &App{}
 }
 
 // startup is called when the app starts. The context is saved
-// so we can call the runtime methods
+// so we can call the runtime methods.
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
+// Greet returns a greeting for the given name.
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// ShowAbout displays the application's about information.
+func (a *App) ShowAbout(data *menu.CallbackData) {
+	runtime.MessageDialog(a.ctx, runtime.MessageDialogOptions{
+		Title:   "About",
+		Message: about.Info(),
+	})
 }
--- blog-writer/main.go
+++ blog-writer/main.go
@@ -1,9 +1,11 @@
+// Copyright (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 package main
 
 import (
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -12,10 +14,15 @@ import (
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
+	// Create an instance of the app structure.
 	app := NewApp()
 
-	// Create application with options
+	// Create application menu.
+	appMenu := menu.NewMenu()
+	helpMenu := appMenu.AddSubmenu("Help")
+	helpMenu.AddText("About", nil, app.ShowAbout)
+
+	// Create application with options.
 	err := wails.Run(&options.App{
 		Title:  "blog-writer",
 		Width:  1024,
@@ -23,6 +30,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		Menu:             appMenu,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
